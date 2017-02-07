13b8a2e29c7128716d8fbdbc84a3cf61c0ed0a60
a15711 <a15711@d1-08-06.dped.local>
2017-02-07 16:24:44 +0000

    Ficha de trabalho 2

--- src/m17b_fichatrab2/Main.java
+++ src/m17b_fichatrab2/Main.java
@@ -1,11 +1,26 @@
 package m17b_fichatrab2;
 
+/**
+ * Utilização do github
+ * @author a15711 | a13228
+ */
+
 public class Main {
 
+        
     public static void main(String[] args) {
         // Ficha de Trabalho #2
+        
+        /**
+         * Inicialização de p1 e p2 do tipo Ponto2D 
+         */
+        
         Ponto2D p1 = new Ponto2D(5,7);
         Ponto2D p2 = new Ponto2D(2,8);
+        
+        /**
+         * É imprimido os valores de p1, p2 e distância  
+         */
         
         System.out.println(p1);
         System.out.println(p2);
--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -3,17 +3,64 @@ package m17b_fichatrab2;
 public class Ponto2D {
     private int x;
     private int y;
+    private int resultado;
     
-    // Faltam construtor, getters e setters.
+    /**
+     * Construtor Ponto2D sem parâmetros
+     */
+    public Ponto2D(){
+        x = 0;
+        y = 0;        
+    }
     
+    /**
+     * Construtor Ponto2D com parâmetros
+     */
     public Ponto2D(int x, int y){
         this.x = x;
         this.y = y;        
     }
     
+    /**
+     * Setter de X
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    /**
+     * Setter de Y
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    /**
+     * Getter de X
+     */
+    public int getX() {
+        return x;
+    }
+    
+    /**
+     * Getter de Y
+     */
+    public int getY() {
+        return y;
+    }
+    
+    
+    /**
+     * Método distancia que calcula a 
+     * distância entre os dois pontos
+     */
     public double distancia(Ponto2D a){
         // Falta fazer este método.
-        return 0.0;
+        if(x<=y)
+            resultado = y-x;
+        else
+            resultado = x-y;
+        return resultado;
     }
     
 }
